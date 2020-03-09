9d669b94f8ed04e6e2e61513be07dfd122d5b743
Katerina Diodorova <aktivirovka@mail.ru>
2020-03-09 20:14:09 +0300

    Adding driver executable address to the code

--- DiodoravaWebdriver/src/test/java/Tests/BaseTest.java
+++ DiodoravaWebdriver/src/test/java/Tests/BaseTest.java
@@ -3,14 +3,28 @@ package Tests;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+import java.io.File;
+import java.net.URL;
 
 abstract public class BaseTest {
    protected WebDriver driver;
 
     @Before
     public void createDriver() {
-        driver = new FirefoxDriver();
+        //driver = new FirefoxDriver();
+        //driver.manage().window().maximize();
+
+        URL url = this.getClass().getClassLoader().getResource("drivers/geckodriver.exe");
+        File file = new File(url.getFile()); // Strangely, URL.getFile does not return a File
+        GeckoDriverService.Builder bldr = (new GeckoDriverService.Builder())
+                .usingDriverExecutable(file)
+                .usingAnyFreePort();
+        driver = new FirefoxDriver(bldr.build());
         driver.manage().window().maximize();
     }
 
